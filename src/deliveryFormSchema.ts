ae59ef7ad9ad4faefef8cda0f77b44e1203edae2
@Enge-Olate <marcioalexisolate@live.com>
2026-03-20 12:18:13 -0300

    Ajustando código Yup e limpando carrinho após sucesso da compra.

--- src/deliveryFormSchema.ts
+++ src/deliveryFormSchema.ts
@@ -1,14 +1,33 @@
 import * as yup from 'yup';
 export const deliveryFormSchema = yup.object({
-    delivery:yup.object({
-        receiver:yup.string().required("O nome do recebedor é obrigatório."),
-        address: yup.object({
-            description: yup.string().required("Descrição é obriratória."),
-            city:yup.string().required("Cidade é obrigatória."),
-            zipCode:yup.string().required("Cep é obrigatório."),
-            numberHouse:yup.number().required("Número é obrigatório.").typeError("Apenas números!").integer("Números sem casas decimais!").min(1, "Número precisa ser maior que zero!"),
-            complement:yup.string().nullable().transform((value)=>value??"").default("")
-        }),
+    delivery: yup.object({
+        receiver:
+            yup.string()
+                .required("O nome do recebedor é obrigatório."),
+        address:
+            yup.object({
+                description:
+                    yup.string()
+                        .required("Descrição é obriratória."),
+                city:
+                    yup.string()
+                        .required("Cidade é obrigatória."),
+                zipCode:
+                    yup.string()
+                        .required("Cep é obrigatório.")
+                        .matches(/^[0-9]{5}-?[0-9]{3}$/, "CEP inválido, verifique!"),
+                numberHouse:
+                    yup.number()
+                        .required("Número é obrigatório.")
+                        .typeError("Apenas números!")
+                        .integer("Números sem casas decimais!")
+                        .min(1, "Número precisa ser maior que zero!"),
+                complement:
+                    yup.string()
+                        .nullable()
+                        .transform((value) => value ?? "")
+                        .default("")
+            }),
     }),
 });
 export type DeliveryFormData = yup.InferType<typeof deliveryFormSchema>;
--- src/paymentFormSchema.ts
+++ src/paymentFormSchema.ts
@@ -1,16 +1,37 @@
 import * as yup from "yup";
 export const paymentFormSchema = yup.object({
     payment: yup.object({
-        card:yup.object({
-            name:yup.string().required("Nome no cartão é obrigatório"),
-            number:yup.string().required("Número do cartão é obrigatório"),
-            code:yup.number().required("CVV é obrigatório").min(3),
+        card: yup.object({
+            name:
+                yup.string()
+                    .required("Nome do cartão é obrigatório."),
+            number:
+                yup.string()
+                    .required("Número do cartão é obrigatório."),
+            code:
+                yup.number()
+                    .required("CVV é obrigatório.")
+                    .min(9, "Verifique o número correto!")
+                    .max(9999, "Verifique o número correto!")
+                    .integer("O código não pode ter casas decimais.")
+                    .typeError("Apenas números."),
         }),
-        expires:yup.object({
-            month:yup.number().required("Campo obrigatório").min(1).max(12),
-            year:yup.number().required("Campo obrigatório").min(4),
+        expires: yup.object({
+            month:
+                yup.number()
+                    .required("Campo obrigatório")
+                    .min(1, "O mês deve estar entre 1 e 12.")
+                    .max(12, "O mês deve estar entre 1 e 12.")
+                    .typeError("Apenas números!"),
+            year:
+                yup.number()
+                    .required("Campo obrigatório.")
+                    .integer()
+                    .min(new Date().getFullYear(), "Verifique!")
+                    .max(9999, "Deve ter quatro dígitos!")
+                    .typeError("Ano inválido!"),
 
         }),
     }),
 });
-export type PaymentFormData = yup.InferType< typeof paymentFormSchema>;
+export type PaymentFormData = yup.InferType<typeof paymentFormSchema>;
